20deedbd2defe44f4d0b8d97fb39851fcdbc920d
Eldar Zakirov <unixod@gmail.com>
2021-05-09 11:05:49 +0300

    Use cmake-modules optionaly through FectchContent (if building tests is necessary)

--- tests-config.cmake
+++ tests-config.cmake
@@ -1,29 +1,42 @@
 include_guard()
 
 include(FetchContent)
-FetchContent_Declare(
-    Catch2
+
+###########################################
+# Fetch dependencies
+###########################################
+FetchContent_Declare(Catch2
     GIT_REPOSITORY git@github.com:unixod/Catch2.git
-    GIT_TAG fix-warnings
+    GIT_TAG e140ebc142836fbda199d80a6593a126e6d29a2f # branch: fix-warnings (hash is used to prevent fetching each time cmake is run)
     GIT_SHALLOW On
 )
 
-FetchContent_MakeAvailable(Catch2)
+FetchContent_Declare(unixod-cmake-modules
+    GIT_REPOSITORY git@github.com:unixod/cmake-modules.git
+    GIT_TAG cf11eb12630ccce9fcab08ce360d34a9e4606c3f
+    GIT_SHALLOW On
+)
 
-#add_subdirectory(third-party/Catch2)
-add_library(ez::tests_config ALIAS Catch2WithMain)
+FetchContent_MakeAvailable(
+    Catch2
+    unixod-cmake-modules
+)
 
-list(APPEND CMAKE_MODULE_PATH
-    ${CMAKE_SOURCE_DIR}/cmake)
+###########################################
+# Add ez::tests_config
+###########################################
+add_library(ez_tests_config INTERFACE)
+add_library(ez::tests_config ALIAS ez_tests_config)
 
 # Enforce /EHsc in case compiler flags were overriten by CMAKE_CXX_FLAGS
 # e.g. see: https://github.com/skvadrik/re2c/pull/339#issuecomment-752357341
 if(MSVC)
-    target_compile_options(Catch2WithMain
+    target_compile_options(ez_tests_config
         PUBLIC
-
         /EHsc
     )
 endif()
 
-include(addTest)
+target_link_libraries(ez_tests_config
+    INTERFACE
+    Catch2WithMain)
