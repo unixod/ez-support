9517cc478c730d144127e80428d17ae723d956fe
Eldar Zakirov <unixod@gmail.com>
2021-05-10 13:07:22 +0300

    Add definitions.h

---
+++ include/ez/support/definitions.h
@@ -0,0 +1,27 @@
+#ifndef EZ_SUPPORT_DEFINITIONS_H
+#define EZ_SUPPORT_DEFINITIONS_H
+
+#define STRINGIFY1(x) #x
+#define STRINGIFY(x) STRINGIFY1(x)
+
+#ifdef NDEBUG
+#   define ERR_MSG
+#else
+#   define ERR_MSG __FILE__ ":" STRINGIFY(__LINE__) ": "
+#endif
+
+#if defined(_MSC_VER)
+#   define UNRECHABLE \
+        assert(false && "Unreachable is reached"); \
+        __assume(0)
+#elif defined(__GNUC__) || defined(__clang__)
+#   define UNRECHABLE \
+        assert(false && "Unreachable is reached"); \
+        __builtin_unreachable()
+#else
+#   warning "UNRECHABLE macro may have no effect due to unable to detect used compiler."
+#   define UNRECHABLE \
+        assert(false && "Unreachable is reached");
+#endif
+
+#endif // EZ_SUPPORT_DEFINITIONS_H
