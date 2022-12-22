3df252fa248d346e12afdb392e63af21f3db4a79
Eldar Zakirov <unixod@gmail.com>
2022-12-22 22:18:49 +0300

    Split compile parameters into public and private subsets.

--- dependencies.cmake
+++ dependencies.cmake
@@ -2,6 +2,15 @@ include_guard()
 
 include(FetchContent)
 
+FetchContent_Declare(ez-common
+    GIT_REPOSITORY https://github.com/unixod/ez-common.git
+    GIT_TAG v1.0.0
+    GIT_SHALLOW On
+)
+
+FetchContent_MakeAvailable(ez-common)
+
+
 if(EZ_SUPPORT_STANDALONE AND BUILD_TESTING)
     FetchContent_Declare(ez-catch
         GIT_REPOSITORY https://github.com/unixod/ez-catch.git
--- tests-config.cmake
+++ tests-config.cmake
@@ -1,12 +1,14 @@
 include_guard()
 
-###########################################
-# Add ez::tests_config
-###########################################
+###############################################################################
+# Define ez::tests_config
+###############################################################################
+
 add_library(ez_support_tests_config INTERFACE)
 add_library(ez::support::tests_config ALIAS ez_support_tests_config)
 
 target_link_libraries(ez_support_tests_config
     INTERFACE
     ez::catch
+    ez::support::private_compile_options
 )
